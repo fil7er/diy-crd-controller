1f841a37b14f969185be26b02279337e8311da56
fil7er <fil7er@gmail.com>
2022-07-21 15:36:51 -0400

    Dockerfile and set class

--- src/CreateController.php
+++ src/CreateController.php
@@ -1,8 +1,9 @@
 <?
+require '../vendor/autoload.php';
 use RenokiCo\PhpK8s\KubernetesCluster;
 
 // Create a new instance of KubernetesCluster.
-$cluster = KubernetesCluster::fromUrl('http://127.0.0.1:8080');
+$cluster = KubernetesCluster::fromUrl('http://127.0.0.1:' . $_ENV["CLUSTER_PORT"]);
 
 // Create a new NGINX service.
 $svc = $cluster->service()
