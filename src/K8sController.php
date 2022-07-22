22458538cfdb598312f8b1481bf1079e6f019cf9
fil7er <fil7er@gmail.com>
2022-07-22 13:04:12 -0400

    Update K8sController.php

--- src/K8sController.php
+++ src/K8sController.php
@@ -13,7 +13,7 @@ class K8sController
     {
         // Create a new instance of KubernetesCluster.
         $this->cluster = KubernetesCluster::fromUrl('http://127.0.0.1:8080'); //set kubectl proxy first
-        $this->cluster->namespace()->setName('staging')->create();
+        $this->cluster->namespace()->setName('dummy')->create();
         $this->cluster->isSynced(); // true
 
     }
@@ -52,6 +52,7 @@ class K8sController
             ->setName($deployName)
             ->setImage('httpd', '2.4')
             ->setEnv(['DummySite' => $webContent])
+            ->setCommand(['echo', $webContent, ">>", "/usr/local/apache2/htdocs/index.html"])
             ->setPorts([
                 ['name' => $deployName, 'protocol' => 'TCP', 'containerPort' => 80],
             ]);
